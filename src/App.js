66dd8c29cb1c5c8a9691cf5e903aa12d437f5096
fakeyatogod <fakeyatokami@gmail.com>
2022-01-15 21:02:31 +0530

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi!</h1>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+      <Route exact path="/starred">
+        This is starred page
+      </Route>
+      <Route>This is 404 page</Route>
+    </Switch>
   );
 }
 
